6d8d24860feec9843ae54ca98f0d57231209f54e
Jeremie Dimino <jeremie@dimino.org>
2010-01-22 09:33:12 +0100

    added an user manual
    Ignore-this: ab0282f3ccfc5b532f24148b29eecc94
    
    darcs-hash:20100122083312-c41ad-498e146433e3b33eee9a9b96a73386f2c20992fe

--- predist.sh
+++ predist.sh
@@ -3,5 +3,12 @@
 # Generate CHANGES.darcs
 [ -d "$DARCS_REPO" ] && darcs changes --repodir "$DARCS_REPO" > CHANGES.darcs
 
+# Build the user manual for release
+cd manual
+make manual.pdf
+# Remove intermediate files
+make clean-aux
+cd ..
+
 # Remove this file
 rm -f predist.sh
